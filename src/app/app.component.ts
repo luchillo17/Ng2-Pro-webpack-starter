5754b8d30704e4158e10a0eac8a7980515b8c68d
Carlos Esteban Lopez <luchillo17@gmail.com>
2016-06-17 13:02:42 -0500

    Init app folder
    Config:
    - Angular 2 hmr
    - Deprecated router
    - App routes example

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+/*
+ * Angular 2 decorators and services
+ */
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouteConfig, Router } from '@angular/router-deprecated';
+
+import { AppState } from './app.service';
+import { Home } from './home';
+import { RouterActive } from './router-active';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  pipes: [ ],
+  providers: [ ],
+  directives: [ RouterActive ],
+  encapsulation: ViewEncapsulation.None,
+  styles: [
+    require('./app.scss')
+  ],
+  template: `
+    <span router-active>
+      <button [routerLink]=" ['Index'] ">
+        Index
+      </button>
+    </span>
+    <span router-active>
+      <button [routerLink]=" ['Home'] ">
+        Home
+      </button>
+    </span>
+    <span router-active>
+      <button [routerLink]=" ['About'] ">
+        About
+      </button>
+    </span>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+  `
+})
+@RouteConfig([
+  { path: '/',      name: 'Index', component: Home, useAsDefault: true },
+  { path: '/home',  name: 'Home',  component: Home }
+  // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
+  // { path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About') }
+])
+export class App {
+  angularclassLogo = 'assets/img/angularclass-avatar.png';
+  loading = false;
+  name = 'Angular 2 Webpack Starter';
+  url = 'https://twitter.com/AngularClass';
+
+  constructor(
+    public appState: AppState) {
+
+  }
+
+  ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }
+
+}
